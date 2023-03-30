a957122e2b8977130cf8e1e19170195532b3abea
Thomas Mol <email@thomasmol.com>
2023-03-30 16:48:29 +0200

    Fix for test

--- test.py
+++ test.py
@@ -2,12 +2,20 @@
 # Run it with `python3 test.py``
 
 import requests
+import os
+
+# the file is a base64 encoded audio file of a few people talking about the moonlanding
+# the morning after it happened (1969)
+
+# open uploads/textbase64.txt and save to a var
+with open('uploads/textbase64.txt', 'r') as f:
+    base64file = f.read()
 
 model_payload = {
-    "file":"somebase64string",
+    "file":base64file,
     "filename":"somefile.mp3",
-    "prompt":"some audio",
-    "num_speakers": 2
+    "prompt":"people talking about the moonlanding",
+    "num_speakers": 3
 }
 
 res = requests.post("http://localhost:8000/",json=model_payload)
